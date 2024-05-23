54f7df513a100aad134bc651f11971eff1c54b0e
jhcha0822 <jhcha0822@naver.com>
2024-05-23 13:11:17 +0900

    24.05.22 관리자 페이지(CMSSettings) 프로젝트 생성

---
+++ CMSApp-settings/src/main/java/com/sds/cmssettings/Controller/MainController.java
@@ -0,0 +1,15 @@
+package com.sds.cmssettings.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String getMain() {
+		return "settings/general";
+	}
+	
+	
+}
