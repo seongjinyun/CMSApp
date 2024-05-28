7ab7ed36ec0fad2090ec67da8ae51ade585306ea
seongjinyun <ckwdktek12@gmail.com>
2024-05-28 14:37:57 +0900

    d

---
+++ CMSApp-main/src/main/java/com/sds/cmsapp/controller/SettingsController.java
@@ -0,0 +1,37 @@
+package com.sds.cmsapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SettingsController {	
+	@GetMapping("/settings/general")
+	public String getGeneral() {
+		return "settings/general";
+	}
+	
+	@GetMapping("/settings/log")
+	public String getLog() {
+		return "settings/log";
+	}
+	
+	@GetMapping("/settings/access")
+	public String getAccess() {
+		return "settings/access";
+	}
+	
+	@GetMapping("/settings/mypage")
+	public String getMypageInfo() {
+		return "settings/mypage";
+	}
+	
+	@GetMapping("/settings/user")
+	public String getUserInfo() {
+		return "settings/user";
+	}
+	
+	@GetMapping("/settings/role")
+	public String getRole() {
+		return "settings/role";
+	}
+}
