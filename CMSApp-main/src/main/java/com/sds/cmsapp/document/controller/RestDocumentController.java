17aa129b3d66a2014d8550e4c37cf7c474c25d8d
jhppgit <jhpp0105@gmail.com>
2024-06-04 11:44:12 +0900

    수정중. 에러있음

--- CMSApp-main/src/main/java/com/sds/cmsapp/document/controller/RestDocumentController.java
+++ CMSApp-main/src/main/java/com/sds/cmsapp/document/controller/RestDocumentController.java
@@ -1,24 +1,31 @@
 package com.sds.cmsapp.document.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sds.cmsapp.domain.Document;
 import com.sds.cmsapp.domain.DocumentRequest;
+import com.sds.cmsapp.domain.Folder;
 import com.sds.cmsapp.domain.VersionLog;
 import com.sds.cmsapp.model.document.DocumentService;
+import com.sds.cmsapp.model.folder.FolderService;
 
 @RestController
 public class RestDocumentController {
 	
 	@Autowired
 	private DocumentService documentService;
+	
+	@Autowired
+	private FolderService folderService;
 	
 	@PostMapping("/document/save")
 	public ResponseEntity createDocument(@ModelAttribute DocumentRequest documentRequest) {
@@ -42,4 +49,10 @@ public class RestDocumentController {
             return new ResponseEntity<>(headers, HttpStatus.FOUND); // 302 Found 상태 반환
         }
     }
+	
+	@GetMapping("document/folder/list")
+	private ResponseEntity getFolderList() {
+		List<Folder> folderList = folderService.selectAll();
+		return null;
+	}
 }
