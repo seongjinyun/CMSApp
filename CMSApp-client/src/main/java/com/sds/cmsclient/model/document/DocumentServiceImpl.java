052f1ed96f5fb1cdb0c172ae83701b9e9855c618
[SeongJin Yun] <[ckwdktek12@gmail.com]>
2024-06-18 23:57:35 +0900

    24_06_18 [클라이언트] - 프로젝트 조회, 폴더 조회, 파일 조회, 파일 상세 조회 완료 - published_version 테이블 기준으로 배포 완료된 문서만 불러오도록 지정 - summernote 에디터에서 저장한 html태그 그대로 적용

--- CMSApp-client/src/main/java/com/sds/cmsclient/model/document/DocumentServiceImpl.java
+++ CMSApp-client/src/main/java/com/sds/cmsclient/model/document/DocumentServiceImpl.java
@@ -1,22 +1,66 @@
 package com.sds.cmsclient.model.document;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sds.cmsclient.domain.Document;
 import com.sds.cmsclient.domain.Folder;
+import com.sds.cmsclient.domain.PublishedVersion;
+import com.sds.cmsclient.domain.VersionLog;
 
 @Service
-public class DocumentServiceImpl implements DocumentService{
-	
-	@Autowired
-	private DocumentDAO documentDAO;
-	
-	public List<Folder> topFolderSelect(int projectIdx) {
-		return documentDAO.topFolderSelect(projectIdx);
+public class DocumentServiceImpl implements DocumentService {
+
+    @Autowired
+    private DocumentDAO documentDAO;
+
+    public List<Folder> getFolderListByProjectIdx(int projectIdx) {
+        List<Folder> folderList = documentDAO.getFolderListByProjectIdx(projectIdx);
+        
+        // 상위 폴더 객체 참조 설정
+        for (Folder folder : folderList) {
+            if (folder.getParentFolderId() != null) {
+                folder.setParentFolder(findParentFolder(folderList, folder.getParentFolderId()));
+            }
+
+        }
+        
+        List<Folder> rootFolders = folderList.stream()
+                .filter(f -> f.getParentFolderId() == null)
+                .collect(Collectors.toList());
+        setChildFoldersAndDocuments(rootFolders, folderList);
+        return rootFolders;
+    }
+
+    private void setChildFoldersAndDocuments(List<Folder> parentFolders, List<Folder> allFolders) {
+        for (Folder parent : parentFolders) {
+            List<Folder> childFolders = allFolders.stream()
+                    .filter(f -> f.getParentFolderId() != null && f.getParentFolderId().equals(parent.getFolderIdx()))
+                    .collect(Collectors.toList());
+            parent.setChildFolders(childFolders);
+            setChildFoldersAndDocuments(childFolders, allFolders);
+            // 해당 폴더의 문서 목록 설정
+            List<PublishedVersion> publishedVersions = documentDAO.getPublishedVersionByDocumentIdx(parent.getFolderIdx());
+            parent.setPublishedVersionList(publishedVersions);
+        }
+    }
+
+    private Folder findParentFolder(List<Folder> folderList, Integer parentFolderId) {
+        return folderList.stream()
+                .filter(f -> f.getFolderIdx().equals(parentFolderId))
+                .findFirst()
+                .orElse(null);
+    }
+    
+    public List<PublishedVersion> getPublishedVersionByDocumentIdx(int documentIdx) {
+        return documentDAO.getPublishedVersionByDocumentIdx(documentIdx);
+    }
+    
+	public String projectNameSelect(int projectIdx) {
+		return documentDAO.projectNameSelect(projectIdx);
 	}
-//	public int subFolderSelect(int folderIdx) {
-//		return documentDAO.subFolderSelect(folderIdx);
-//	}
+
 }
