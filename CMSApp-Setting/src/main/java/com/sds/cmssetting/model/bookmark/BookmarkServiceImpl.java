bf4a73bee80d721a85dd3780d359189c09b8aed7
jhppgit <jhpp0105@gmail.com>
2024-06-20 20:22:37 +0900

    CMS Setting 에서 사용하지 않는 서비스 정리

--- CMSApp-Setting/src/main/java/com/sds/cmssetting/model/bookmark/BookmarkServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.sds.cmssetting.model.bookmark;
-
-public class BookmarkServiceImpl {
-
-}
--- CMSApp-Setting/src/main/java/com/sds/cmssetting/model/publishing/MainPublishingService.java
+++
@@ -1,9 +0,0 @@
-package com.sds.cmssetting.model.publishing;
-
-import com.sds.cmssetting.domain.RequestPublishingDTO;
-
-public interface MainPublishingService {
-	
-	public void publishDoc(RequestPublishingDTO publishingDTO);
-
-}
