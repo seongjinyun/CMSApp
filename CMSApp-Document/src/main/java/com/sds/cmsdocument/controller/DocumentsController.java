6500544fa0a809223b7bcf5b4e7e8a4b06f37086
jhppgit <jhpp0105@gmail.com>
2024-06-21 10:09:35 +0900

    폴더 옮기기, 생성 예외 추가(Restored v

--- CMSApp-Document/src/main/java/com/sds/cmsdocument/controller/DocumentsController.java
+++ CMSApp-Document/src/main/java/com/sds/cmsdocument/controller/DocumentsController.java
@@ -97,6 +97,7 @@ public class DocumentsController {
 			Collections.reverse(parentFolderList);
 			model.addAttribute("parentFolderList", parentFolderList);
 			model.addAttribute("subFolderList", subFolderList);
+			model.addAttribute("restoredFolderIdx", folderService.selectRestoreFolder().getFolderIdx());
 			return "documents/list";
 		}
 	} 
