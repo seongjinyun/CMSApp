1b1616763be098c188c02dd6660d7b486c7351a2
최지인 <chris309804@gmail.com>
2024-06-23 14:58:07 +0900

    dashboard publishing 메뉴 - writer 접근 못하게 처리

--- CMSApp-Document/src/main/java/com/sds/cmsdocument/dashboard/controller/DocumentController.java
+++ CMSApp-Document/src/main/java/com/sds/cmsdocument/dashboard/controller/DocumentController.java
@@ -1,12 +1,25 @@
 package com.sds.cmsdocument.dashboard.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import com.sds.cmsdocument.domain.Emp;
+import com.sds.cmsdocument.jwt.JwtValidService;
 
 @Controller(value="dashboardDocumentController")
 public class DocumentController {
+	
+	@Autowired
+	JwtValidService jwtValidService;
 
-	@GetMapping("/admin/dashboard/activity")
+	@GetMapping("/dashboard/activity")
 	public String getAdminDashboardActivity() {
 		return "dashboard/activity";
 	}
@@ -16,4 +29,30 @@ public class DocumentController {
 		return "dashboard/publishing";
 	}
 	
+	@GetMapping("/emp/check")
+	public ResponseEntity<?> getEmpIdx(@RequestHeader(name="Authorization") String header) {
+		String token = header.replace("Bearer ", "");
+		Emp emp = jwtValidService.getEmpFromJwt(token);
+	    String roleName = emp.getRole().getRoleName();
+		Map<String, String> response = new HashMap<>();
+		response.put("empIdx", ""+emp.getEmpIdx());
+		response.put("roleName", roleName);
+		return ResponseEntity.ok(response);
+	}
+	
+	@GetMapping("/checkAuthority/admin/dashboard/publishing")
+	public ResponseEntity<?> checkAuthorityForMypage(@RequestHeader(name="Authorization") String header) {
+	    String token = header.replace("Bearer ", "");
+	    Emp emp = jwtValidService.getEmpFromJwt(token);
+	    String roleName = emp.getRole().getRoleName();
+	    
+	    Map<String, String> response = new HashMap<>();
+	    if(roleName.equals("Admin")) {
+	        response.put("url", "/admin/dashboard/publishing");
+	        return ResponseEntity.ok(response);
+	    } else {
+	        response.put("url", "/error");
+	        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
+	    }
+	}
 }
