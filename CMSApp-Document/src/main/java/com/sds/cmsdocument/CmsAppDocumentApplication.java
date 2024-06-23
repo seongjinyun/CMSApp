d7351a2313677e4b3d21585c37f6696f50f8daf8
최지인 <chris309804@gmail.com>
2024-06-24 00:40:03 +0900

    war 빌드 관련 설정 내용 추가

--- CMSApp-Document/src/main/java/com/sds/cmsdocument/CmsAppDocumentApplication.java
+++ CMSApp-Document/src/main/java/com/sds/cmsdocument/CmsAppDocumentApplication.java
@@ -2,12 +2,19 @@ package com.sds.cmsdocument;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class CmsAppDocumentApplication {
+public class CmsAppDocumentApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CmsAppDocumentApplication.class);
+	}	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CmsAppDocumentApplication.class, args);
--- CMSApp-client/src/main/java/com/sds/cmsclient/CmsAppClientApplication.java
+++ CMSApp-client/src/main/java/com/sds/cmsclient/CmsAppClientApplication.java
@@ -2,12 +2,19 @@ package com.sds.cmsclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class CmsAppClientApplication {
+public class CmsAppClientApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CmsAppClientApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CmsAppClientApplication.class, args);
 	}
