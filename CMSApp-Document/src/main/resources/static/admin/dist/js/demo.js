15e38216fea5eadf29a86eb02c5ffefb74e32747
jhcha0822 <jhcha0822@naver.com>
2024-06-23 21:15:51 +0900

    24.06.23 PM21:15

--- CMSApp-Document/src/main/resources/static/admin/dist/js/demo.js
+++ CMSApp-Document/src/main/resources/static/admin/dist/js/demo.js
@@ -9,7 +9,7 @@
 
 (function ($) {
   'use strict'
-
+/*
   setTimeout(function () {
     if (window.___browserSync___ === undefined && Number(localStorage.getItem('AdminLTE:Demo:MessageShowed')) < Date.now()) {
       localStorage.setItem('AdminLTE:Demo:MessageShowed', (Date.now()) + (15 * 60 * 1000))
@@ -17,7 +17,7 @@
       alert('You load AdminLTE\'s "demo.js", \nthis file is only created for testing purposes!')
     }
   }, 1000)
-
+*/
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1)
   }
